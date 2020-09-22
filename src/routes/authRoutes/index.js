7484ce6a397f618ed85365e10ef7fec06383bb46
Corbin Taylor <cptaylor38@yahoo.com>
2020-09-22 18:58:51 -0400

    added singin and pass hashing

--- src/routes/authRoutes/index.js
+++ src/routes/authRoutes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const db = require('../../models');
 const jwt = require('jsonwebtoken');
+const { User } = require('../../models');
 
 router.post('/signup', async (req, res) => {
-  console.log(req.body);
   const { email, password } = req.body;
   try {
     const user = new db.User({ email, password });
@@ -12,6 +12,26 @@ router.post('/signup', async (req, res) => {
     res.send({ token });
   } catch (err) {
     return res.status(422).send(err.message);
+  }
+});
+
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: 'Must provide an email and password.' });
+  }
+  const user = await db.User.findOne({ email });
+  if (!user) {
+    return res.status(404).send({ error: 'Invalid password or email.' });
+  }
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, 'secret_key');
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: 'Invalid password or email.' });
   }
 });
 
